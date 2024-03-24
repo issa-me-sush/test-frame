a1e14a398d8154ae4008962d908609be5ef42cc7
issa-me-sush <coolsushaan@gmail.com>
2024-03-24 10:20:47 +0530

    wrong contract address

--- app/config.ts
+++ app/config.ts
@@ -1,4 +1,4 @@
 // use NODE_ENV to not have to change config based on where it's deployed
 export const NEXT_PUBLIC_URL =
   process.env.NODE_ENV == 'development' ? 'http://localhost:3000' : 'https://test-frame-two.vercel.app';
-export const AD_CONTRACT_ADDR = '0xcD3D5E4E498BAb2e0832257569c3Fd4AE439dD6f';
+export const AD_CONTRACT_ADDR = '0x2feEdc2a029F2d925A9b6dc6c54D669cCca5d849';
